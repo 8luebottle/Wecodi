58581c870056c85628212d95ceda64eb1da1f58d
8luebottle <itbiz.irs@gmail.com>
2019-11-05 22:41:25 +0900

    MODIFY MODEL : users(password)

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from .views import(
+    SignUpView
+)
 
 urlpatterns = [
+    path('', SignUpView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import bcrypt
+import datetime
+import json
+import jwt
+import requests
 
-# Create your views here.
+from django.http  import HttpResponse, JsonResponse
+from django.views import View
+
+"""
+SignUp
+LogIn
+    - Email
+    - Kakao
+    - Google
+    - Facebook
+LogOut
+Delete Account
+Change PW
+Profile
+    - Create
+    - Update
+    - View
+User List View (Admin)
+    - All
+    - Blocked
+    - Unblocked
+Blocking User View (Admin)
+"""
+
+class SignUpView(View): # User SignUp
+    def post(self, request):
+        request_dict = json.loads(request.body)
+        print(request_dict)
+
+        """
+            new_user = (
+                email      : request_dict['email']
+                first_name : request_dict['first_name']
+                last_name  : request_dict['last_name']
+            ).create()
+        """
+
+        return JsonResponse({'message' : 'SUCCESS'})
+
