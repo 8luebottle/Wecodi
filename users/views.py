60ec836c5093e1f42d5f994a16093a2967ce1831
8luebottle <itbiz.irs@gmail.com>
2019-11-05 23:07:24 +0900

    WIP

--- users/views.py
+++ users/views.py
@@ -7,6 +7,8 @@ import requests
 from django.http  import HttpResponse, JsonResponse
 from django.views import View
 
+from .models import User, Profile
+
 """
 SignUp
 LogIn
@@ -25,7 +27,7 @@ User List View (Admin)
     - All
     - Blocked
     - Unblocked
-Blocking User View (Admin)
+Blocking User View (Admin) --> UserStatusView
 """
 
 class SignUpView(View): # User SignUp
@@ -33,13 +35,45 @@ class SignUpView(View): # User SignUp
         request_dict = json.loads(request.body)
         print(request_dict)
 
-        """
-            new_user = (
-                email      : request_dict['email']
-                first_name : request_dict['first_name']
-                last_name  : request_dict['last_name']
-            ).create()
-        """
+        new_user = User(
+            email      = request_dict['email'],
+            password   = request_dict['password'],
+            first_name = request_dict['first_name'],
+            last_name  = request_dict['last_name']
+        ).save()
 
         return JsonResponse({'message' : 'SUCCESS'})
+
+
+class LoginView(View): # User Login with their email
+    pass
+
+
+class KakaoLoginView(View):
+    pass
+
+
+class GoogleLoginView(View):
+    pass
+
+
+class FacebookLoginView(View):
+    pass
+
+
+class UserListView(View): #ADMIN ONLY (Need decorator)
+    pass
+
+
+class BlockUserView(View): # ADMIN Block user
+    pass
+
+
+class UserSatatusView(View): #Segmentation by User Status
+    pass
+
+
+class ProfileView(View): # User & Admin can Update their Profile
+    pass
+
 
